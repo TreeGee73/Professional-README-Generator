aadb596fd1ae5ab5c6c895ba30714824a7765498
TreeGee73 <TreeGee73@gmail.com>
2020-12-03 21:44:24 -0700

    initial questions array build

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('.utils/generateMarkdown');
+
+
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project\'s name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'usefaq',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'contibutefaq',
+        message: 'What does the user need to know about contributing to the repo?'
+    },
 
 ];
 
